30425cb35509cbdcd465751cc60af7169a405b5c
tamarasaurus <tamara@robichet.com>
2018-06-26 23:24:02 +0200

    Scrapers fail when all values of a selector are false

--- scraper/src/test/run-test.ts
+++ scraper/src/test/run-test.ts
@@ -1,47 +1,69 @@
-import { resolve } from 'path'
+import { resolve, basename } from 'path'
 import * as glob from 'glob'
 import * as puppeteer from 'puppeteer'
 import * as cheerio from 'cheerio'
 import * as assert from 'assert'
 
 const scrape = async (sourcePath: string) => {
+    const sourceName = basename(sourcePath, '.js')
     const sourceModule: any = require(resolve(__dirname, '../', sourcePath))
     const source = sourceModule.default()
 
     if (source.type === 'json') return;
 
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    })
+    try {
+        const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] })
+        const page = await browser.newPage()
+        await page.goto(source.url)
+        await page.waitForSelector(source.resultSelector)
+        const response = await page.content()
+        await browser.close()
 
-    const page = await browser.newPage()
-    await page.goto(source.url)
-    await page.waitForSelector(source.resultSelector)
-    const response = await page.content()
-    await browser.close()
+        const $: CheerioStatic = cheerio.load(response)
+        const results = $(source.resultSelector)
+        const attributes = source.resultAttributes
 
-    const $: CheerioStatic = cheerio.load(response)
-    const results = $(source.resultSelector)
-    const attributes = source.resultAttributes
+        assert(results.length > 0, `${sourceName} - ${source.resultSelector} is empty`)
 
-    assert(results.length > 0, `${sourcePath} - ${source.resultSelector} is empty`)
+        const selectors: any = {}
 
-    results.toArray().forEach((resultElement: CheerioElement) => {
-        attributes.forEach((attribute: any) => {
-            const { type, selector } = attribute
+        results.toArray().forEach((resultElement: CheerioElement) => {
 
-            if (selector && type !== 'photo') {
-                assert(
-                    $(selector, resultElement).length > 0,
-                    `${sourcePath} - ${type} element with selector ${selector} is missing`
-                )
-            }
+            attributes.forEach((attribute: any) => {
+                const type = attribute.type
+                let selector = attribute.selector || source.resultSelector
+
+                if (!selectors[selector]) selectors[selector] = []
+
+                selectors[selector].push({
+                    selector,
+                    type,
+                    isEmpty: $(selector, resultElement).length === 0 && $(selector).length === 0
+                })
+            })
         })
-    })
+
+        for (let i in selectors) {
+            const selector = selectors[i]
+            assert.notEqual(selector.every((val: any) => val.isEmpty === true), `✖ ${sourceName} - ${i}`);
+
+            console.log(`✔ ${sourceName} - ${i}`);
+        }
+
+        console.log('\n')
+    } catch (e) {
+        console.log('Can\'t load', sourcePath, e)
+        process.exit(1)
+    }
+
 }
 
 const sources: String[] = glob.sync(resolve(__dirname, '../source/**/*.js'))
-sources.map(async (sourcePath: string) => scrape(sourcePath))
 
-export default scrape
+console.log('➡️ testing', sources, '\n')
+
+async function test() {
+    await Promise.all(sources.map(async (sourcePath: string) => scrape(sourcePath)))
+}
+
+test()
