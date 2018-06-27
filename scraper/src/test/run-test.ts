2fa931c4d655a61cd2393192f5ec7cdbcc1121a8
Tamara Robichet <tamara.robichet@akeneo.com>
2018-06-27 17:01:10 +0200

    Fix #18 add tests for JSON sources

--- scraper/src/test/run-test.ts
+++ scraper/src/test/run-test.ts
@@ -3,75 +3,107 @@ import * as glob from 'glob'
 import * as puppeteer from 'puppeteer'
 import * as cheerio from 'cheerio'
 import * as assert from 'assert'
+import * as request from 'request-promise'
+import { HTMLSource, JSONSource } from '../types/source';
+
+const testHTMLSource = async (source: HTMLSource, sourcePath: string) => {
+    const sourceName = basename(sourcePath, '.js')
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        pipe: true,
+        timeout: 60000
+    })
+
+    const page = await browser.newPage()
+    await page.goto(source.url)
+    await page.waitForSelector(source.resultSelector, { timeout: 60000 })
+    const response = await page.content()
+    await browser.close()
+
+    const $: CheerioStatic = cheerio.load(response)
+    const results = $(source.resultSelector)
+    const attributes = source.resultAttributes
+
+    assert(results.length > 0, `${sourceName} - ${source.resultSelector} is empty`)
+
+    const selectors: any = {}
+
+    results.toArray().forEach((resultElement: CheerioElement) => {
+
+        attributes.forEach((attribute: any) => {
+            const type = attribute.type
+            let selector = attribute.selector || source.resultSelector
+
+            if (!selectors[selector]) selectors[selector] = []
+
+            selectors[selector].push({
+                selector,
+                type,
+                isEmpty: $(selector, resultElement).length === 0 && $(selector).length === 0
+            })
+        })
+    })
+
+    for (let i in selectors) {
+        const selector = selectors[i]
+
+        assert(
+            !(selector.every((val: any) => val.isEmpty === true)),
+            `✖ ${sourceName} - ${i}`
+        )
+
+        console.log(`✔ ${sourceName} - ${i}`);
+    }
+
+    console.log('\n')
+}
+
+const testJSONSource = async (source: JSONSource, sourcePath: string) => {
+    const sourceName = basename(sourcePath, '.js')
+    const response = await request.get(source.url, { resolveWithFullResponse: true })
+    const contents = JSON.parse(response.body)
+
+    assert(
+        contents.hasOwnProperty(source.resultSelector),
+        `✖ ${sourceName} - ${source.resultSelector}`
+    )
+
+    console.log(`✔ ${sourceName} - ${source.resultSelector}`);
+
+    source.resultAttributes.forEach(attribute => {
+        const firstItem = contents[source.resultSelector][0]
+
+        assert(
+            firstItem.hasOwnProperty(attribute.selector),
+            `✖ ${sourceName} - ${attribute.selector}`
+        )
+
+        console.log(`✔ ${sourceName} - ${attribute.selector}`);
+    })
+
+    console.log('\n')
+}
 
 const scrape = async (sourcePath: string) => {
-    const sourceName = basename(sourcePath, '.js')
     const sourceModule: any = require(resolve(__dirname, '../', sourcePath))
     const source = sourceModule.default()
 
-    if (source.type === 'json') return;
-
     try {
-        const browser = await puppeteer.launch({
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-            pipe: true,
-            timeout: 60000
-        })
-
-        const page = await browser.newPage()
-        await page.goto(source.url)
-        await page.waitForSelector(source.resultSelector, { timeout: 60000 })
-        const response = await page.content()
-        await browser.close()
-
-        const $: CheerioStatic = cheerio.load(response)
-        const results = $(source.resultSelector)
-        const attributes = source.resultAttributes
-
-        assert(results.length > 0, `${sourceName} - ${source.resultSelector} is empty`)
-
-        const selectors: any = {}
-
-        results.toArray().forEach((resultElement: CheerioElement) => {
-
-            attributes.forEach((attribute: any) => {
-                const type = attribute.type
-                let selector = attribute.selector || source.resultSelector
-
-                if (!selectors[selector]) selectors[selector] = []
-
-                selectors[selector].push({
-                    selector,
-                    type,
-                    isEmpty: $(selector, resultElement).length === 0 && $(selector).length === 0
-                })
-            })
-        })
-
-        for (let i in selectors) {
-            const selector = selectors[i]
-
-            if (selector.every((val: any) => val.isEmpty === true)) {
-                console.log(`✖ ${sourceName} - ${i}`);
-                process.exit(1)
-            } else {
-                console.log(`✔ ${sourceName} - ${i}`);
-            }
-
+        if (source.type === 'html') {
+            return await testHTMLSource(source, sourcePath)
         }
 
-        console.log('\n')
+        return await testJSONSource(source, sourcePath)
     } catch (e) {
-        console.log('Can\'t load', sourcePath, e)
+        console.log('\n Tests failed with error: \n\n', sourcePath, '\n', e, '\n')
         process.exit(1)
     }
-
 }
 
 const sources: String[] = glob.sync(resolve(__dirname, '../source/**/*.js'))
 
-console.log('➡️ testing', sources, '\n')
+console.log('➡️ testing \n', sources, '\n')
 
 async function test() {
     await Promise.all(sources.map(async (sourcePath: string) => scrape(sourcePath)))
