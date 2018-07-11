dd86a1bace729e4f4396c1a38f4ee32014fd25a3
Tamara Robichet <tamara.robichet@akeneo.com>
2018-07-11 11:41:03 +0200

    Add cases to size tests

--- scraper/src/test/helper/size.ts
+++ scraper/src/test/helper/size.ts
@@ -1,3 +1,7 @@
+import * as assert from 'assert'
+import { getSize } from '../../helper/size'
+
 export default function() {
-    console.log('test size')
+    assert.equal(getSize('the size is 123m² squared and number 10'), 123)
+    assert.equal(getSize('the house is 10 m²'), 10)
 }
