abad1916f50dea2e12e633f586c3032f89f18ab0
Tamara Robichet <tamara.robichet@akeneo.com>
2018-07-27 11:59:16 +0200

    Add down migration for multiple photos

--- scraper/migrations/20180727084829-add-photos.js
+++ scraper/migrations/20180727084829-add-photos.js
@@ -2,7 +2,7 @@ module.exports = {
   up(db) {
     return db.collection('results').aggregate([
       { $project: {
-        "photos": ['$photo'],
+        photos: ['$photo'],
         date: 1,
         name: 1,
         price: 1,
@@ -15,9 +15,20 @@ module.exports = {
     ]).toArray()
   },
 
-  down(db, next) {
-    // TODO write the statements to rollback your migration (if possible)
-    next()
+  down(db) {
+    return db.collection('results').aggregate([
+      { $project: {
+        photo: { $arrayElemAt: [ "$photos", 0 ]},
+        date: 1,
+        name: 1,
+        price: 1,
+        size: 1,
+        description: 1,
+        link: 1,
+        hidden: 1
+      }},
+      { $out: "results" }
+    ]).toArray()
   }
 
 }
