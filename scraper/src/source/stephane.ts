525dc6f450850ae0f43b392872b21735585d574b
Tamara Robichet <tamara.robichet@akeneo.com>
2018-08-05 20:47:22 +0200

    Add multiple images for stephane plaza

--- scraper/src/source/stephane.ts
+++ scraper/src/source/stephane.ts
@@ -1,7 +1,9 @@
 import { HTMLSource } from '../types/html-source'
 
+const baseUrl = 'http://www.stephaneplazaimmobilier-nantesest.com'
+
 export default class Stephane extends HTMLSource {
-    public url = 'http://www.stephaneplazaimmobilier-nantesest.com/catalog/result_carto.php?action=update_search&map_polygone=&C_28=Vente&C_28_search=EGAL&C_28_type=UNIQUE&site-agence=&C_65_search=CONTIENT&C_65_type=TEXT&C_65=44+NANTES&C_65_temp=44+NANTES&cfamille_id=&C_33_search=COMPRIS&C_33_type=TEXT&C_33_MIN=&C_33_MAX=&C_30_search=INFERIEUR&C_30_type=NUMBER&C_30=&C_30_format_quick='
+    public url = `${baseUrl}/catalog/result_carto.php?action=update_search&map_polygone=&C_28=Vente&C_28_search=EGAL&C_28_type=UNIQUE&site-agence=&C_65_search=CONTIENT&C_65_type=TEXT&C_65=44+NANTES&C_65_temp=44+NANTES&cfamille_id=&C_33_search=COMPRIS&C_33_type=TEXT&C_33_MIN=&C_33_MAX=&C_30_search=INFERIEUR&C_30_type=NUMBER&C_30=&C_30_format_quick=`
     public resultSelector = '[data-product-id]'
 
     public resultAttributes = [
@@ -16,14 +18,26 @@ export default class Stephane extends HTMLSource {
                 const style = $(photo).attr('style')
                 let imageUrl = /(background-image: url\(")(.*)("\))/gm.exec(style)[2]
                 imageUrl = imageUrl.split('../')[1]
-                return [`http://www.stephaneplazaimmobilier-nantesest.com/${imageUrl}`]
+                return [`${baseUrl}/${imageUrl}`]
             }
         },
         {
             type: 'link',
             format($: CheerioStatic, link: CheerioElement): string {
                 const url = $(link).data('url').split('?')[0]
-                return `http://www.stephaneplazaimmobilier-nantesest.com${url}`
+                return `${baseUrl}${url}`
+            }
+        }
+    ]
+
+    public richAttributes = [
+        {
+            type: 'photos',
+            selector: '.slides li > img',
+            format($: CheerioStatic, photos: CheerioElement): string[] {
+                return $(photos).map((index: number, photo: CheerioElement) => {
+                    return `${baseUrl}/${$(photo).attr('src')}`
+                }).get()
             }
         }
     ]
