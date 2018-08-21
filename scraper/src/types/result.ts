40e6e8fbf7335a85c6ea59c637645b3520b85395
Tamara Robichet <tamara.robichet@akeneo.com>
2018-08-21 17:29:20 +0200

    Add additional details to Result type

--- scraper/src/types/result.ts
+++ scraper/src/types/result.ts
@@ -1,5 +1,26 @@
+type Rating = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g'
+type Type = 'apartment' | 'house' | 'studio'
+
+class ContactDetails {
+    name: string
+    address: string
+    siret: string
+    siren: string
+    email: string
+    number: string
+}
+
 class ResultDetails {
     photos: string[]
+    type: Type
+    rooms: number
+    ges: Rating
+    class_energie: Rating
+    furnished: boolean
+    fees: number
+    charges_included: boolean
+    contact: ContactDetails
+    location: string
 }
 
 export class Result {
