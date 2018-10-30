9c053c099b8de70330aaf962767c1b99443e6149
Tamara Robichet <tamara.robichet@akeneo.com>
2018-10-30 21:29:13 +0100

    Use sequelize with psql

--- scraper/src/processors/store.ts
+++ scraper/src/processors/store.ts
@@ -1,11 +1,16 @@
-import { Storage } from '../storage/mongo'
+import { Storage } from '../storage/postgres'
 import { Job, DoneCallback } from 'bull';
 
 module.exports = async function(job: Job, done: DoneCallback) {
     try {
         const storage = new Storage()
         const results = job.data
-        const storedResults = await storage.updateOrCreate(results)
+        const storedResults = []
+
+        for (let result of results) {
+            storedResults.push(await storage.updateOrCreate(result))
+        }
+
         storage.cleanup()
         done(null, storedResults)
     } catch (e) {
--- scraper/src/storage/postgres.ts
+++ scraper/src/storage/postgres.ts
@@ -5,7 +5,7 @@ export class Storage {
   public result: any;
 
   constructor() {
-    this.database = new Sequelize('immo-feed', 'postgres', 'postgres', {
+    this.database = new Sequelize('postgres', 'postgres', 'postgres', {
       host: process.env.POSTGRES_HOST,
       dialect: 'postgres',
 
@@ -17,37 +17,45 @@ export class Storage {
       },
     });
 
-    this.result = Sequelize.define('result', {
-      date: Date,
-      name: String,
-      price: { type: Number },
-      size: Number,
-      description: String,
-      link: { type: String, unique: true },
-      photo: String,
-      hidden: { type: Boolean, default: false },
+    this.result = this.database.define('result', {
+      name: Sequelize.STRING,
+      price: Sequelize.INTEGER,
+      size: Sequelize.FLOAT,
+      description: Sequelize.TEXT,
+      link: {
+        type: Sequelize.STRING,
+        primaryKey: true
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+      photo: Sequelize.STRING,
+      hidden: { type: Sequelize.BOOLEAN, defaultValue: false }
     });
 
-    console.log(this.database)
+
+    this.result.sync()
   }
 
   findById(id: string) {
-
+    return this.result.findById(id)
   }
 
-  findAll(page: string = '1', filter = '', sort = ['date', 'desc']) {
-
+  findAll(page: string = '1', filter = '', sort = ['date', 'DESC']) {
+    return this.result.findAll({ hidden: false })
   }
 
-  updateOrCreate(results: any) {
-
+  async updateOrCreate(result: any) {
+    return this.result.findOrCreate({
+        where: { link: result.link },
+        defaults: result
+      })
   }
 
   cleanup() {
-
+    this.database.close()
   }
 
   findUpdatedSince(date: Date) {
-
+    return this.result.findAll()
   }
 }
