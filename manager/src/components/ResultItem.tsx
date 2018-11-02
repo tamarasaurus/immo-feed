8a13dd5c0aef3cf6d169ef1f6919f36ade97914b
Tamara Robichet <tamara.robichet@akeneo.com>
2018-11-02 22:36:21 +0100

    Make grid card sizes consistent

--- manager/src/components/ResultItem.tsx
+++ manager/src/components/ResultItem.tsx
@@ -6,7 +6,18 @@ interface ResultItemProps {
   onUnpin: (id: string) => any
 }
 
-class ResultItem extends Component<ResultItemProps> {
+interface ResultItemState {
+  expanded: boolean
+}
+
+class ResultItem extends Component<ResultItemProps, ResultItemState> {
+  constructor(props: ResultItemProps) {
+    super(props)
+    this.state = {
+      expanded: false
+    }
+  }
+
   onPin(event: any) {
     this.props.onPin(this.props.data.id)
   }
@@ -15,17 +26,34 @@ class ResultItem extends Component<ResultItemProps> {
     this.props.onUnpin(this.props.data.id)
   }
 
+  toggleExpanded() {
+    this.setState({expanded: !this.state.expanded})
+  }
+
   render() {
     const { link, photo, name, createdAt, price, size, description, pinned } = this.props.data
+    const style = { 'backgroundImage': `url(${photo})`}
+    const trimmedDescription = description.slice(0, 150)
+    const trimmed = trimmedDescription.length < description.length
+
     return (
       <div key={link} className="result">
-        <a href={link}> <img src={photo} /> </a>
+        <a className="result-link" href={link} style={style}></a>
         <div className="result-info">
           <span className="column column-full">
             <div className="result-title">{name}</div>
             <div className="result-date">{new Date(`${createdAt}`).toLocaleString()}</div>
             <div className="result-details">€{price.toLocaleString()} | {size}m²</div>
-            <div className="result-description">{description}</div>
+            <div className="result-description">
+              {
+                this.state.expanded ?
+                <span>
+                  {description} <button title="Collapse" className="result-expand" onClick={this.toggleExpanded.bind(this)}>⇱</button>
+                </span> :
+                <span>
+                  {trimmedDescription}{ trimmed ? <>...<button title="Expand" className="result-expand" onClick={this.toggleExpanded.bind(this)}> ⇲</button></> : ''}
+                </span> }
+            </div>
           </span>
           <span className="result-actions">
             <div onClick={pinned ? this.onUnpin.bind(this) : this.onPin.bind(this)} className={`result-action good ${pinned ? 'active' : ''}`}>⚪</div>
