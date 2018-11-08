fa2a0a16de1ce7424a0bfa1da891a29a5279d147
Tamara Robichet <tamara.robichet@akeneo.com>
2018-11-08 17:47:03 +0100

    Add bottom pagination
    Fix row styles
    
    Add filters dropdown

--- manager/src/index.tsx
+++ manager/src/index.tsx
@@ -5,6 +5,8 @@ import "antd/lib/grid/style/css";
 import "antd/lib/form/style/css";
 import "antd/lib/button/style/css";
 import "antd/lib/pagination/style/css";
+import "antd/lib/dropdown/style/css";
+import "antd/lib/menu/style/css";
 import './index.css';
 
 import App from './App';
