08cc722440ed60e729381939cce8757dc4853efd
Tamara Robichet <tamara.robichet@akeneo.com>
2018-11-22 18:12:51 +0100

    Implement update on conflict for upsert

--- api/routes/result/actions/upsert.ts
+++ api/routes/result/actions/upsert.ts
@@ -2,26 +2,48 @@ import db from "../../../db";
 import { Response, Request } from "express";
 
 export default function(request: Request, response: Response, next: any) {
-  const { name, price, size, description, link, photo } = request.body;
+  const {
+    name,
+    price,
+    size,
+    description,
+    link,
+    photo,
+    hidden,
+    pinned,
+    seen
+  } = request.body;
+
+  console.log('undefined', hidden, pinned, seen)
 
   // @TODO - Make request data into a validated object
-  // @TODO - Implement upsert (so accept all post data)
-  //{
-  //     "name": "Appartement",
-  //     "price": 55000,
-  //     "size": 1,
-  //     "description": "Griasdfdsdgny",
-  //     "link": "https://gasdfasdsdfsdlesd.c3om",
-  //     "created": null,
-  //     "updated": null,
-  //     "photo": "https://v.seloger.com/s/crop/310x225/visuels/0/b/t/k/google.com"
-  // }
-
+  // @TODO - Dynamically structure the query to only insert not null values in the update
   db.query(`
-    INSERT INTO results( name, price, size, description, link, photo )
+    INSERT INTO results(name, price, size, description, link, photo)
     VALUES ($1, $2, $3, $4, $5, $6 )
+    ON CONFLICT (link)
+    DO
+      UPDATE
+        SET name = $1,
+            price = $2,
+            size = $3,
+            description = $4,
+            photo = $6,
+            updated = now()
+            ,hidden = $7,
+            pinned = $8,
+            seen = $9
   `,
-    [ name, price, size, description, link, photo ])
+    [ name,
+      price,
+      size,
+      description,
+      link,
+      photo,
+      hidden,
+      pinned,
+      seen
+    ])
   .then(() => {
     response.status(200).send('OK')
   })
