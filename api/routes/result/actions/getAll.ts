b3b002787c0b76203d6fd8acd92b4d2d7fc49d38
Tamara Robichet <tamara.robichet@akeneo.com>
2018-11-23 19:35:55 +0100

    Implement filters by search and price in results

--- api/routes/result/actions/getAll.ts
+++ api/routes/result/actions/getAll.ts
@@ -2,17 +2,40 @@ import db from '../../../db'
 import { QueryResult } from 'pg'
 import { Response, Request } from 'express'
 
-export default function(request: Request, response: Response, next: any) {
-  const { offset } = request.params
-
-  console.log('offset', offset)
+export default function (request: Request, response: Response, next: any) {
+  const {
+    min_price,
+    max_price,
+    min_size,
+    max_size,
+    offset,
+    limit
+  } = request.query
 
   db.query(`
-    SELECT * FROM results
+    SELECT *
+    FROM results
+    WHERE price BETWEEN COALESCE($1, (
+      SELECT MIN(price) from results
+    )) AND COALESCE($2, (
+      SELECT MAX(price) from results
+    ))
+    AND size BETWEEN COALESCE($3, (
+      SELECT MIN(size) from results
+    )) AND COALESCE($4, (
+      SELECT MAX(size) from results
+    ))
     ORDER BY created DESC
-    LIMIT 10
-    OFFSET $1
-  `, [ (offset || 0) ])
+    LIMIT COALESCE($6, 10)
+    OFFSET COALESCE($5, 0)
+  `, [
+      min_price,
+      max_price,
+      min_size,
+      max_size,
+      offset,
+      limit
+    ])
     .then((result: QueryResult) => {
       response.send(result.rows)
     })
