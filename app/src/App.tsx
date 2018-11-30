b98c27bee955ba8dae9d844429287151a8af6cdd
Tamara Robichet <tamara.robichet@akeneo.com>
2018-11-30 16:24:55 +0100

    Split result components

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,8 @@ import React, { Component, MouseEvent, ChangeEvent } from 'react'
 import Pagination from './components/Pagination'
 import { getAll, getPinned, getFilters, saveResult } from './services/Result'
 import { DebounceInput } from 'react-debounce-input';
+import PinnedResult from './components/PinnedResult';
+import Result from './components/Result';
 
 interface AppState {
   results: any[]
@@ -27,6 +29,7 @@ class App extends Component<{}, AppState> {
 
     this.offsetUpdated = this.offsetUpdated.bind(this)
     this.pinResult = this.pinResult.bind(this)
+    this.unpinResult = this.unpinResult.bind(this)
     this.searchUpdated = this.searchUpdated.bind(this)
   }
 
@@ -94,27 +97,8 @@ class App extends Component<{}, AppState> {
         <>
           <h2>Pinned</h2>
           <ul className="list">
-            {this.state.pinned.map((result) => {
-                return <li
-                    className="list-item"
-                    key={result.id} id={result.id}>
-                    <a href={result.link} target="_blank" className="list-item-photo" style={{backgroundImage: `url('${result.photo}')`}}></a>
-                    <div className="list-item-details">
-                      <div className="list-item-name">{result.name}</div>
-                      <div className="list-item-date">{new Date(result.created).toLocaleDateString()} {new Date(result.created).toLocaleTimeString()}</div>
-                      <div className="list-item-description">{result.description}</div>
-                    </div>
-                    <div className="list-item-data">
-                      {result.size > 0 ? <span className="pill">{result.size}m²</span> : ''}
-                      {result.price > 0 ? <span className="pill">€{result.price.toLocaleString()}</span> : ''}
-                    </div>
-                    <div className="list-item-actions">
-                      <button onClick={this.unpinResult.bind(this, result)} className="action hover"><svg viewBox="0 0 32 32" aria-hidden="true"><path fill="#ccc" d="M16 4.588l2.833 8.719H28l-7.416 5.387 2.832 8.719L16 22.023l-7.417 5.389 2.833-8.719L4 13.307h9.167L16 4.588z"/></svg></button>
-                      <button className="action"><svg viewBox="0 0 32 32" aria-hidden="true"><path fill="#ccc" d="M16 5C9.935 5 5 9.934 5 16c0 6.067 4.935 11 11 11s11-4.933 11-11c0-6.066-4.935-11-11-11zm0 2.75c1.777 0 3.427.569 4.775 1.53L9.279 20.778A8.214 8.214 0 0 1 7.75 16c0-4.549 3.701-8.25 8.25-8.25zm0 16.5a8.2 8.2 0 0 1-4.775-1.53l11.494-11.497A8.205 8.205 0 0 1 24.25 16c0 4.547-3.701 8.25-8.25 8.25z"/></svg></button>
-                    </div>
-                </li>
-                })}
-            </ul>
+            {this.state.pinned.map((result) =><PinnedResult key={result.id} result={result} unpinResult={this.unpinResult}/>)}
+          </ul>
         </>
         : ''
       }
@@ -122,26 +106,7 @@ class App extends Component<{}, AppState> {
           <>
           <h2>Results</h2>
           <ul className="list">
-            {this.state.results.map((result) => {
-                return <li
-                    className="list-item"
-                    key={result.id} id={result.id}>
-                    <a href={result.link} target="_blank" className="list-item-photo" style={{backgroundImage: `url('${result.photo}')`}}></a>
-                    <div className="list-item-details">
-                      <div className="list-item-name">{result.name}</div>
-                      <div className="list-item-date">{new Date(result.created).toLocaleDateString()} {new Date(result.created).toLocaleTimeString()}</div>
-                      <div className="list-item-description">{result.description}</div>
-                    </div>
-                    <div className="list-item-data">
-                      {result.size > 0 ? <span className="pill">{result.size}m²</span> : ''}
-                      {result.price > 0 ? <span className="pill">€{result.price.toLocaleString()}</span> : ''}
-                    </div>
-                    <div className="list-item-actions">
-                      <button onClick={this.pinResult.bind(this, result)}className="action"><svg viewBox="0 0 32 32" aria-hidden="true"><path fill="#ccc" d="M16 4.588l2.833 8.719H28l-7.416 5.387 2.832 8.719L16 22.023l-7.417 5.389 2.833-8.719L4 13.307h9.167L16 4.588z"/></svg></button>
-                      <button className="action"><svg viewBox="0 0 32 32" aria-hidden="true"><path fill="#ccc" d="M16 5C9.935 5 5 9.934 5 16c0 6.067 4.935 11 11 11s11-4.933 11-11c0-6.066-4.935-11-11-11zm0 2.75c1.777 0 3.427.569 4.775 1.53L9.279 20.778A8.214 8.214 0 0 1 7.75 16c0-4.549 3.701-8.25 8.25-8.25zm0 16.5a8.2 8.2 0 0 1-4.775-1.53l11.494-11.497A8.205 8.205 0 0 1 24.25 16c0 4.547-3.701 8.25-8.25 8.25z"/></svg></button>
-                    </div>
-                </li>
-            })}
+            {this.state.results.map((result) => <Result key={result.id} result={result} pinResult={this.pinResult} />)}
           </ul>
         </>
       : <div className="empty">No results</div> }
