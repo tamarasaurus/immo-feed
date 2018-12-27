3ae3faeaf826abf361a2e861436e241703b2af36
Tamara Robichet <tamara.robichet@akeneo.com>
2018-12-27 16:18:33 +0100

    Add export files

--- app/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'react-input-range/lib/css/index.css'
-import './index.css'
-
-import App from './App'
-
-ReactDOM.render(<App />, document.getElementById('root'))
