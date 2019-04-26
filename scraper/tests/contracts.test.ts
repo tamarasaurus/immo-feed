71eb5acf4a24795624ab0a232f6e2b2a8c8fde0d
Tamara Robichet <tamara.robichet@akeneo.com>
2019-04-26 13:43:48 +0200

    Add automated tests for contract selectors

--- scraper/tests/contracts.test.ts
+++ scraper/tests/contracts.test.ts
@@ -1,20 +1,37 @@
-import scrape from '../src/jobs/scrape'
-import * as assert from 'assert'
-import { Job, DoneCallback } from 'bull';
+import Scraper from 'contract-scraper';
+import * as glob from 'glob'
+import * as path from 'path'
+import * as sites from '../src/sites.json';
+import StephaneLink from '../src/attributes/StephaneLink';
+import * as assert from 'assert';
 
-const Ouestfrance = require('../src/contracts/Ouestfrance.json')
+const contractList = glob.sync('src/contracts/*.json')
+const scrapingJobs = contractList.map((contractPath) => {
+    const baseName = path.basename(contractPath)
+    const fileName = baseName.replace(path.extname(baseName), '');
 
+    const url = sites.find((site) => site.type === fileName).url;
+    const contract = require(`../${contractPath}`)
 
-const job: any = {
-    data: {
-        url: 'https://www.ouestfrance-immo.com/acheter/nantes-44-44000/?types=maison,appartement',
-        contract: Ouestfrance
+    return {
+        site: fileName,
+        scraper: new Scraper(url, contract, { 'stephane-link': StephaneLink }),
+        attributes: contract.attributes
     }
-}
-const callback: DoneCallback = (error, scrapedItems: any[]) => {
-    console.log('done', scrapedItems)
+});
 
-}
+for (let job of Object.values(scrapingJobs))
+{
+    it(`scrapes ${job.site} selectors`, () => {
+        return job.scraper.scrapePage().then((scrapedItems) => {
+            Object.entries(job.attributes).forEach(([name, attribute]: [string, any]) => {
+                const items = scrapedItems.filter((item) => {
+                    return item.hasOwnProperty(name) && item[name] !== null
+                });
 
-const scrapedItem = scrape(job, callback)
+                assert.equal(items.length > 0, true, `${job.site} is missing the ${name} attribute "${attribute.selector}" - \n\n ${JSON.stringify(scrapedItems[0], null, 2)}`);
+            });
+        }).catch(e => { throw e });
+    })
+}
 
